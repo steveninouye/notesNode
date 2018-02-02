91ea23d8533172d84b05bd039dff81f97611bf78
Steven Inouye <steveninouye@msn.com>
2018-02-01 14:33:04 -1000

    debugging

--- app.js
+++ app.js
@@ -1,24 +1,39 @@
-console.log('Starting app.js');
-
 const fs = require('fs');
 const _ = require('lodash');
 const yargs = require('yargs');
 
-const notes = require('./notes.js')
+const notes = require('./notes.js');
 
 const argv = yargs.argv;
-var command = process.argv[2];
-console.log(`Command: ${command}`);
-console.log('Yargs', argv);
+var command = argv._[0];
 
 if (command === 'add') {
-    notes.addNote(argv.title, argv.body);
+  let note = notes.addNote(argv.title, argv.body);
+  if (note) {
+    console.log('Note created');
+    notes.logNote(note);
+  } else {
+    console.log('Note title taken');
+  }
 } else if (command === 'list') {
-    notes.getAll();
+  let allNotes = notes.getAll();
+  console.log(`Printing ${allNotes.length} notes(s)`);
+  allNotes.forEach(e => notes.logNote(e));
 } else if (command === 'read') {
-    notes.getNote(argv.title);
+
+  let note = notes.getNote(argv.title);
+
+  if (note) {
+    console.log('Note read');
+    notes.logNote(note);
+  } else {
+    console.log('Note not found');
+  }
+
 } else if (command === 'remove') {
-    notes.removeNote(argv.title);
+  var noteRemoved = notes.removeNote(argv.title);
+  var message = noteRemoved ? 'Note was removed' : 'Note not found';
+  console.log(message);
 } else {
-    console.log('Command not recognized');
+  console.log('Command not recognized');
 }
